df0b771d813c127d1e00d7e9a62d48b4d49f6324
lucasdanilox <lukastecno@gmail.com>
2024-01-31 23:57:21 -0300

    UserServiceTests

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,31 +1,99 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserDetailsFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
 public class UserServiceTests {
 
-	@InjectMocks
-	private UserService service;
+    @InjectMocks
+    private UserService service;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private CustomUserUtil customUserUtil;
 
-	@Test
-	public void authenticatedShouldReturnUserEntityWhenUserExists() {
-	}
+    private String existingUserName, nonExistingUserName;
+    private UserEntity userEntity;
+    private List<UserDetailsProjection> userDetails;
 
-	@Test
-	public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
-	}
+    @BeforeEach
+    void setUp() throws Exception {
 
-	@Test
-	public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
-	}
+        existingUserName = "maria@gmail.com";
+        nonExistingUserName = "usernotfound@empty.com";
 
-	@Test
-	public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
-	}
+        userEntity = UserFactory.createUserEntity();
+        userDetails = UserDetailsFactory.createCustomAdminUser(existingUserName);
+
+        Mockito.when(userRepository.searchUserAndRolesByUsername(existingUserName)).thenReturn(userDetails);
+        Mockito.when(userRepository.searchUserAndRolesByUsername(nonExistingUserName)).thenReturn(new ArrayList<>());
+        Mockito.when(userRepository.findByUsername(existingUserName)).thenReturn(Optional.of(userEntity));
+        Mockito.when(userRepository.findByUsername(nonExistingUserName)).thenReturn(Optional.empty());
+
+    }
+
+    @Test
+    public void authenticatedShouldReturnUserEntityWhenUserExists() {
+
+        Mockito.when(customUserUtil.getLoggedUsername()).thenReturn(existingUserName);
+
+        UserEntity result = service.authenticated();
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getUsername(), existingUserName);
+
+    }
+
+    @Test
+    public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+
+        Mockito.doThrow(ClassCastException.class).when(customUserUtil).getLoggedUsername();
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            service.authenticated();
+        });
+
+
+    }
+
+    @Test
+    public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
+
+        UserDetails result = service.loadUserByUsername(existingUserName);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getUsername(), existingUserName);
+
+
+    }
+
+    @Test
+    public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            service.loadUserByUsername(nonExistingUserName);
+        });
+
+    }
 }
